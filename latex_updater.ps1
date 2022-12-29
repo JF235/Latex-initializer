94ccff50503c21d5f6da0f3d1231a223a6818526
João Felipe <jfcm.pipe@gmail.com>
2022-12-29 16:42:54 -0300

    Incluindo pasta com configurações predefinidas.

--- latex_updater.ps1
+++ latex_updater.ps1
@@ -1,3 +1,16 @@
 $att = Read-Host -Prompt 'Quer atualizar qual arquivo? (tex|csv)'
 
-python "C:\Users\jfcmp\Coisas do Jota\Code\Python\Latex initializer\latex_initializer.py" -update $att $pwd
+# Camada de segurança na atualização de tex
+$confirm = "n"
+if ($att -eq "tex"){
+    $confirm = Read-Host -Prompt "Tem certeza que deseja atualizar o .tex? O arquivo local sera sobrescrito e o seu conteudo perdido. (y|n)"
+} elseif ($att -eq "csv") {
+    $confirm = "y"
+}
+
+if ($confirm -eq "y"){
+    python "C:\Users\jfcmp\Coisas do Jota\Code\Python\Latex initializer\latex_initializer.py" -update $att $pwd
+    echo "Atualizado."
+} else {
+    echo "Atualizacao cancelada."
+}
